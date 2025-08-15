1bd93c8d803709c334e52997927725cd551a7c43
Oscar Zheng <85961849+ozheng7@users.noreply.github.com>
2025-08-15 00:41:22 -0700

    Fix typo in About section resume button
    Corrected the 'border-primay' class to 'border-primary' in the Download Resume button in the About section. This resolves a styling issue caused by the typo.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { NotFound } from "./pages/NotFound";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/oscar-zheng-portfolio">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
