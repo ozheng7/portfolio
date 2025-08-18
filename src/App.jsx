c4a081e3df675c9b576581093860053ec0b5a237
Oscar Zheng <85961849+ozheng7@users.noreply.github.com>
2025-08-18 08:44:46 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { NotFound } from "./pages/NotFound";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/oscar-zheng-portfolio">
+      <BrowserRouter basename="">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
